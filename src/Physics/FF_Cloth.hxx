c2419e1d6bb76f1204808e4d577b91ec3cb599bb
Ozsbourn <mibaranin@gmail.com>
2022-07-18 19:19:29 +0500

    [FIX]: Fixed logic of cloth entity

--- src/Physics/FF_Cloth.hxx
+++ src/Physics/FF_Cloth.hxx
@@ -50,6 +50,10 @@ namespace FF {
 
         T                          m_LinearDampeningCoefficient;
 
+        inline void HandleCollision( FF::Vector3<T> separationDistance,
+                                     T              changeInTime,
+                                     FF::IndexPair  firstParticle,
+                                     FF::IndexPair  secondParticle );
     public:
         explicit Cloth(void) = delete;
 
@@ -64,9 +68,38 @@ namespace FF {
                         T              __FF_IN linearDampeningFactor,
                         FF::Vector3<T> __FF_IN upLeftCorner );
 
+        inline void           SetParticleImpulseForce(std::size_t row,  std::size_t column, const FF::Vector3<T>& impulseForce);
+        inline FF::Vector3<T> GetParticleImpulseForce(std::size_t row,  std::size_t column);
+        inline void           SetParticleConstantForce(std::size_t row, std::size_t column, const FF::Vector3<T>& constantForce);
+        inline FF::Vector3<T> GetParticleConstantForce(std::size_t row, std::size_t column);
+
+        inline bool isParticleStatic(std::size_t row, std::size_t column);
+        inline void SetParticleStaticFlag(std::size_t row, std::size_t column, bool flag);
+
+        inline bool Update(const T changeInTime);
+        inline bool Render(void);
+
         ~Cloth(void);
     };
 
+    /**
+     * @brief [Constructor that get IS_STATIC flag for particle]
+     * @details [Constructor allocate the memory and set positions and properties according to parameters]
+     * 
+     * @param rows [Row in particle buffer]
+     * @param columns [Columns in particle buffer]
+     * @param particleMass [Mass of one particle]
+     * @param particleRadius [Radius of one particle]
+     * @param particleElasticity [Restitution of particle]
+     * @param spaceBetweenParticles [Space between two particles]
+     * @param clothStiffness [Stiffness for each spring]
+     * @param dampeningFactor [Dampening for each spring]
+     * @param linearDampeningFactor [Linear dampening for cloth]
+     * @param upLeftCorner [Location of up left corner cloth]
+     * @tparam T [Generic type]
+     * 
+     * @return [-]
+     */
     template<typename T>
     inline FF::Cloth<T>::Cloth( std::size_t    __FF_IN rows, 
                                 std::size_t    __FF_IN columns,
@@ -190,6 +223,208 @@ namespace FF {
 
         this->m_TotalRows    = rows;
         this->m_TotalColumns = columns;
+    }
+
+    /**
+     * @brief []
+     * @details [-]
+     * 
+     * @@param row [Row in PARTICLE_BUFFER]
+     * @param column [Column in PARTICLE_BUFFER]
+     * @param impulseForce [Vector3 that represent IMPULSE_FORCE of particle]
+     * @tparam T [Generic Type]
+     * 
+     * @return [-]
+     */
+    template<typename T>
+    inline void FF::Cloth<T>::SetParticleImpulseForce(std::size_t row, std::size_t column, const FF::Vector3<T>& impulseForce){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+        
+        this->m_Particles[row][column].SetImpulseForce(impulseForce);
+    }
+
+    /**
+     * @brief [Method that get impulse force (i, j) particle from PARTICLE_BUFFER]
+     * @details [-]
+     * 
+     * @param row [Row in PARTICLE_BUFFER]
+     * @param column [Column in PARTICLE_BUFFER]
+     * @param impulseForce [Vector3 that represent IMPULSE_FORCE of particle]
+     * @tparam T [Generic Type]
+     * 
+     * @return [Return impulse force of (i, j) particle in PARTICLE_BUFFER]
+     */
+    template<typename T>
+    inline FF::Vector3<T> FF::Cloth<T>::GetParticleImpulseForce(std::size_t row, std::size_t column){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+        
+        return (this->m_Particles[row][column].GetImpulseForce());
+    }
+
+    /**
+     * @brief []
+     * @details [-]
+     * 
+     * @param row [Row in PARTICLE_BUFFER]
+     * @param column [Column in PARTICLE_BUFFER]
+     * @param impulseForce [Vector3 that represent CONSTANT_FORCE of particle]
+     * @tparam T [Generic Type]
+     * 
+     * @return [-]
+     */
+    template<typename T>
+    inline void FF::Cloth<T>::SetParticleConstantForce(std::size_t row, std::size_t column, const FF::Vector3<T>& constantForce){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+        
+        this->m_Particles[row][column].SetConstantForce(constantForce);
+    }
+
+    /**
+     * @brief [Method that get constant force of (i, j) particle in PARTICLE_BUFFER]
+     * @details [-]
+     * 
+     * @param row [Row in PARTICLE_BUFFER]
+     * @param column [Column in PARTICLE_BUFFER]
+     * @param impulseForce [Vector3 that represent CONSTANT_FORCE of particle]
+     * @tparam T [Generic Type]
+     * 
+     * @return [Return constant force of (i, j) particle in PARTICLE_BUFFER]
+     */
+    template<typename T>
+    inline FF::Vector3<T> FF::Cloth<T>::GetParticleConstantForce(std::size_t row, std::size_t column){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+        
+        return (this->m_Particles[row][column].GetConstantForce());
+    }
+
+    /**
+     * @brief [Method that get IS_STATIC flag for particle]
+     * @details [-]
+     * 
+     * @param row [Row in particle buffer]
+     * @param column [Column in particle buffer]
+     * @tparam T [Generic type]
+     * 
+     * @return [Flag that represent move ability for particle]
+     */
+    template<typename T>
+    inline bool FF::Cloth<T>::isParticleStatic(std::size_t row, std::size_t column){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+
+        return this->m_Particles[row][column].isStatic();
+    }
+
+    /**
+     * @brief [Method that set IS_STATIC flag for particle]
+     * @details [-]
+     * 
+     * @param row [Row in particle buffer]
+     * @param column [Column in particle buffer]
+     * @tparam T [Generic type]
+     * 
+     * @return [-]
+     */
+    template<typename T>
+    inline void FF::Cloth<T>::SetParticleStaticFlag(std::size_t row, std::size_t column, bool flag){
+        FF_ASSERT_MESSAGE((row >= 0x0 && row < this->m_TotalRows), "Going beyond particle buffer! Check row index!");
+        FF_ASSERT_MESSAGE((column >= 0x0 && column < this->m_TotalColumns), "Going beyond particle buffer! Check column index!");
+
+        this-m_Particles[row][column].SetStaticFlag();
+    }
+
+    /**
+     * @brief []
+     * @details []
+     * 
+     * @param changeInTime []
+     * @return []
+     */
+    template<typename T>
+    inline bool FF::Cloth<T>::Update(const T changeInTime){
+        std::size_t i,j;
+
+        // Calculate the force exerted by each spring
+        for (i = 0x0; i < this->m_TotalSprings; i++) {
+            if (this->m_Springs[i].IsDisplaced()) {
+                this->m_Springs[i].CalculateReactions(changeInTime);
+            }
+        }
+
+        // Update the position of every particle in the grid.
+        for (i = 0x0; i < this->m_TotalRows; i++) {
+            for (j = 0x0; j < this->m_TotalColumns; j++) {
+                for (std::size_t k = i; k < this->m_TotalColumns; k++) {
+                    for (std::size_t m = j + 0x1; m < this->m_TotalColumns; m++) {
+                        // Find the distance vector between the particles.
+                        FF::Vector3<T> distance = this->m_Particles[i][j].GetLocation() - this->m_Particles[k][m].GetLocation();
+                        T distanceSquared = FF::SquaredMagnitude(distance);
+
+                        // Find the square of the sum of the radii of the balls.
+                        T minDistanceSquared = this->m_Particles[i][j].GetBoundingSphereRadius() + this->m_Particles[k][m].GetBoundingSphereRadius();
+                        minDistanceSquared *= minDistanceSquared;
+
+                        // If there is a collision
+                        if (distanceSquared < minDistanceSquared) {
+                            IndexPair firstParticle, secondParticle;
+                            firstParticle.m_row     = i;
+                            firstParticle.m_column  = j;
+                            secondParticle.m_row    = k;
+                            secondParticle.m_column = m;
+
+                            // Handle the collision.
+                            HandleCollision(distance, changeInTime, firstParticle, secondParticle);
+                        }
+                    }
+                }
+            }
+        }
+
+        //
+        /* This is cheating. Spring-particle systems are notoriously
+        unstable. To make this behave more like cloth, dampen the 
+        movement of the particles in the system.*/
+        //
+        FF::Vector3<T> dampening;
+        for (i = 0x0;i < this->m_TotalRows; i++) {
+            for (j = 0x0; j < this->m_TotalColumns; j++) {
+                dampening = -this->m_linearDampeningCoefficient * this->m_Particles[i][j].GetLinearVelocity();
+                this->m_Particles[i][j].SetLinearVelocity(this->m_Particles[i][j].GetLinearVelocity() + dampening);
+            }
+        }
+
+        // Update each ball.
+        for (i = 0x0; i < this->m_TotalRows; i++) {
+            for (j = 0x0; j < this->m_TotalColumns; j++) {
+                this->m_Particles[i][j].Update(changeInTime);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @brief [Method that render cloth]
+     * @details [Render each of particle that consists in cloth]
+     * 
+     * @tparam T [Generic type]
+     * @return [Return status of render cloth]
+     */
+    template<typename T>
+    inline bool FF::Cloth<T>::Render(void){
+        bool RenderStatus = true;
+
+        for (std::size_t i = 0x0; RenderStatus && i < this->m_TotalRows; i++) {
+            for (int j=0; RenderStatus && j < this->m_TotalColumns; j++) {
+                RenderStatus = this->m_Particles[i][j].Render();
+            }
+        }
+
+        return (RenderStatus);
     }
 };
 
